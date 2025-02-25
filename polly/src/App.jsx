2fea2a503102ff3d66815b3788772a93a64c20a7
Mr <jpihlk@yahoo.com>
2025-02-24 19:11:00 -0500

    added branch that just returns the audio right away - no S3 bucket needed

--- polly/src/App.jsx
+++ polly/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
-import { S3Client } from "@aws-sdk/client-s3";
-import { Upload } from "@aws-sdk/lib-storage";
+
 const AWS_SECRET_ACCESS_KEY = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
 const AWS_ACCESS_KEY_ID = import.meta.env.VITE_AWS_ACCESS_KEY_ID;
 const AWS_REGION = import.meta.env.VITE_AWS_REGION;
 const BUCKET_NAME = import.meta.env.VITE_BUCKET_NAME;
  
 function App() {
+  const [audioUrl, setAudioUrl] = useState(null);
   const [text, setText] = useState("");
 
-  const synthesizeAudio = async (sourceDestinationConfig) => {
+  const handleSynthesize = async (text) => {
     const pollyClient = new PollyClient({
       region: AWS_REGION,
       credentials: {
@@ -22,63 +22,40 @@ function App() {
 
     const synthesizeSpeechCommand = new SynthesizeSpeechCommand({
       Engine: "neural",
-      Text: sourceDestinationConfig.translated_text,
+      Text: text,
       VoiceId: "Ruth",
       OutputFormat: "mp3",
     });
 
-    const { AudioStream } = await pollyClient.send(synthesizeSpeechCommand);
-
-    const audioKey = `${sourceDestinationConfig.object}.mp3`;
-
-    // Store the audio file in S3.
-    const s3Client = new S3Client({
-      region: AWS_REGION,
-      credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY
-      }
-    });
-    const upload = new Upload({
-      client: s3Client,
-      params: {
-        Bucket: BUCKET_NAME,
-        Key: audioKey,
-        Body: AudioStream,
-        ContentType: "audio/mp3",
-        CORSHeaders: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      },
-    });
-
-    await upload.done();
-    return audioKey;
-  };
-
-  const handleSynthesize = async () => {
     try {
-      const result = await synthesizeAudio({
-        bucket: BUCKET_NAME,
-        translated_text: text,
-        object: "unique-file-name",
-      });
-      console.log("Audio file created:", result);
+      const response = await pollyClient.send(synthesizeSpeechCommand);
+      const audioArray = await response.AudioStream.transformToByteArray()
+      const blob = new Blob([audioArray], { type: 'audio/mpeg' });
+      
+      if (audioUrl) {
+        URL.revokeObjectURL(audioUrl);
+      }
+      
+      const url = URL.createObjectURL(blob);
+      setAudioUrl(url);
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  return <>
-  <textarea 
+  return (
+    <>
+      <textarea 
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Enter text to synthesize"
       />
-      <button onClick={handleSynthesize}>
+      <button onClick={() => handleSynthesize(text)}>
         Synthesize Speech
       </button>
-  </>;
+      {audioUrl && <audio src={audioUrl} controls />}
+    </>
+  );
 }
 
 export default App;
